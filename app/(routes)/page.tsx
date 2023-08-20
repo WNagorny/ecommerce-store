146035df96d69915d3468da6a94e1d8c72744da5
WNagorny <walery.nagorny@gmail.com>
2023-08-20 14:10:10 +0200

    added billboard component and added actions for it

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,24 @@
-const HomePage = () => {
-   return ( 
-      <div>
-         Hello Store
+
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+   const billboard = await getBillboard("815cfe01-6ee3-4d45-9462-cdc9e307289d")
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          
+        B</div>
       </div>
-    );
-}
- 
+    </Container>
+  )
+};
+
 export default HomePage;
