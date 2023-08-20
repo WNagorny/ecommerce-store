4fb9c6767fb6db91fec3e45efea7abee54cb1a40
WNagorny <walery.nagorny@gmail.com>
2023-08-20 14:13:13 +0200

    updated types for Products

--- types.ts
+++ types.ts
@@ -1,4 +1,31 @@
 
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[]
+};
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+};
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+};
+
+export interface Image {
+  id: string;
+  url: string;
+}
  export interface Billboard {
    id: string;
    label: string;
