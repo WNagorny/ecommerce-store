04471e09a9104e616b8777b0d8ba55245d2ca2f4
WNagorny <walery.nagorny@gmail.com>
2023-08-20 13:27:45 +0200

    creating routes with Billboards and Categories

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,9 +5,10 @@ import Link from "next/link"
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils"
+import { Category } from "@/types";
 
 interface MainNavProps {
-   data: any
+   data: Category[];
  }
 
 
