f1646802445555d47931ae460d67008d9a0924d4
WNagorny <walery.nagorny@gmail.com>
2023-08-20 14:49:52 +0200

    added product-card / icon-button components

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,7 @@
 
 import { Product } from "@/types";
 import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 interface ProductListProps {
   title: string;
@@ -17,7 +18,7 @@ const ProductList: React.FC<ProductListProps> = ({
       {items.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map((item) => (
-          <div>{item.name}</div>
+          <ProductCard key={item.id} data={item}/>
         ))}
       </div>
     </div>
