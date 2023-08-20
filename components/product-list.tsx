d5c896b0f94f90d6c4983ee3bdb0900af26e4412
WNagorny <walery.nagorny@gmail.com>
2023-08-20 14:30:36 +0200

    added no-results component

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,6 @@
 
 import { Product } from "@/types";
+import NoResults from "@/components/ui/no-results";
 
 interface ProductListProps {
   title: string;
@@ -13,7 +14,12 @@ const ProductList: React.FC<ProductListProps> = ({
   return (
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
-      
+      {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <div>{item.name}</div>
+        ))}
+      </div>
     </div>
    );
 }
