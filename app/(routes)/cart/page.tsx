9ff81fb906f8be7d3f1ae563ac7856a41ade3589
WNagorny <walery.nagorny@gmail.com>
2023-08-23 10:40:16 +0200

    added state to CardPage

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from 'react';
 
 import Container from '@/components/ui/container';
 import useCart from '@/hooks/use-cart';
@@ -10,9 +11,16 @@ import Summary from './components/summary';
 export const revalidate = 0;
 
 const CartPage = () => {
- 
+  const [isMounted, setIsMounted] = useState(false)
   const cart = useCart();
 
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div className="bg-white">
